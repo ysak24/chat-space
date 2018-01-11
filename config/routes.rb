e4b021e6221cf8d10106d6e0fc73a6b6e2d825a5
ysak24 <mitarashi.kushidango99@gmail.com>
2018-01-11 18:51:51 +0900

    add-group-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resource :user, only: [:edit, :update]
+  # root 'messages#index'
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
